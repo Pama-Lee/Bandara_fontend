f89a93b6c4c59dbb6e6b954fa6ebeb7464c9f636
Pama Lee <pama@pamalee.cn>
2023-08-27 00:58:44 +0800

    feat: 新增App Bar背景

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,12 @@
         </v-list>
       </v-navigation-drawer>
   
-      <v-app-bar>
+      <v-app-bar image="https://picsum.photos/1920/1080?random" color="teal-darken-4">
+        <template v-slot:image>
+          <v-img
+              gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
+          ></v-img>
+        </template>
         <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
         <v-app-bar-title>航迹 - Bandara</v-app-bar-title>
       </v-app-bar>
