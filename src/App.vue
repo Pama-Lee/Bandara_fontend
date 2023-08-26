56c2560fd56a622300186798737e242c5d6708b2
GowayLee <1513624626@qq.com>
2023-08-26 20:38:44 +0800

    Feat:"添加了Vuetify"

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
+      <v-btn>Button</v-btn>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +18,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
