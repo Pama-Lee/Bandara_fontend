63dd75b3f6fe79665a000a3aeb9dfa12b87b5f99
GowayLee <1513624626@qq.com>
2023-08-27 13:18:55 +0800

    feat：页面布局与互动
     - HomePage: 添加了随机的模糊(blur)图片作为背景
     - AppBar: 为了避免导航栏背景与主页背景风格差异过大，将原先的随机图片改为固定id图片
     - FlightCard： 航班信息垂直居中
                    减小了鼠标非悬浮于该元素上时该元素的透明度
                    当鼠标进入该元素时透明度增加，阴影(shadow)增加

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
         </v-list>
       </v-navigation-drawer>
   
-      <v-app-bar image="https://picsum.photos/1920/1080?random" color="teal-darken-4">
+      <v-app-bar image="https://picsum.photos/id/558/1920/1080?random" color="teal-darken-4">
         <template v-slot:image>
           <v-img
               gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,22 +1,26 @@
 <template>
-    <v-row class="row-style" justify-sm="center" justify-md="space-between">
-        <v-col lg="8" md="10" sm="12" offset-md="1" offset-lg="2">
+    <v-container class="background-container"> <!--  背景图片 -->
+        <v-row class="row-style" justify-sm="center" justify-md="space-between">
+            <v-col lg="8" md="10" sm="12" offset-md="1" offset-lg="2">
 
-          <!--  搜素框 -->
-          <v-text-field
-              density="compact"
-              variant="solo"
-              label="搜索航班号/出发城市"
-              append-inner-icon="mdi-magnify"
-              single-line
-              hide-details
-          ></v-text-field>
-            <div style="margin-bottom: 20px"></div>
-           <FlightCard />
-          <FlightCard />
-          <FlightCard />
-        </v-col>
-    </v-row>
+            <!--  搜素框 -->
+            <v-text-field
+                density="compact"
+                variant="solo"
+                label="搜索航班号/出发城市"
+                append-inner-icon="mdi-magnify"
+                single-line
+                hide-details
+            ></v-text-field>
+                <div style="margin-bottom: 20px"></div>
+            <FlightCard />
+            <FlightCard />
+            <FlightCard />
+            <FlightCard />
+            <FlightCard />
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script setup lang="ts">
@@ -27,6 +31,11 @@
 
 .row-style {
     margin-top: 20px;
+}
+.background-container {
+  background-image: url("https://picsum.photos/1920/1080?blur");
+  background-size: cover;
+  background-position: center;
 }
 
 </style>
