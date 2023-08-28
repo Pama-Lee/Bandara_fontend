ed50d66853c889fe548d69a417c7e65246cc8f0b
GowayLee <1513624626@qq.com>
2023-08-28 21:17:59 +0800

    fix：修改主页显示

--- src/components/home/FlightCard.vue
+++ src/components/home/FlightCard.vue
@@ -10,8 +10,7 @@
     ">
       <v-row style="height: 100%; margin-left: 5px;"
       justify="center">
-        <v-col sm="4" lg="2" cols="4" align-self="center"
-               style="height: 100%">
+        <v-col cols="2" align-self="center" style="height: 100%">
           <v-img
             src="https://daisycon.io/images/airline/?width=300&height=100&color=ffffff&iata=ak"
             height="100%"
@@ -21,26 +20,28 @@
             style="opacity: 0.9;"
             />
         </v-col>
-        <v-col sm="8" lg="9" cols="8"
-               style="padding: 20px; color: #393939">
-          <p style="font-weight: 800; font-size: 32px; margin-top: 5px">MF1515</p>
-          <v-row no-gutters>
-            <v-col cols="12" md="4" sm="6" align-self="center">
-              <p style="font-size: 13px;  margin-bottom: 5px;">
+        <v-col cols="10" style="padding: 20px; color: #393939">
+          <v-row style="margin-top: 5px;" align-content="center">
+              <span style=" font-weight: bold; font-size: 24px;">MF1515</span>
+
+          </v-row>
+          <v-row style="margin-bottom: 5px;">
+            <v-col  md="5" sm="5" align-self="center">
+              <p style="font-size: 15px;">
                 <v-icon>mdi-airplane-takeoff</v-icon>
-                <span style="margin-left: 15px; font-size: 16px">21:45</span><span style="margin-left: 15px; font-size: 16px">2023/09/06</span>
-              </p>
+                <span style="margin-left: 15px; font-size: 16px;">21:45</span><span style="margin-left: 15px; font-size: 16px;">2023/09/06</span>
+              </p> 
             </v-col>
-            <v-col cols="12" md="4" sm="6" align-self="center">
-              <p style="font-size: 13px;font-weight: bold; margin-bottom: 5px">
+            <v-col  md="4" sm="5" align-self="center">
+              <p style="font-size: 15px;font-weight: bold;">
                 <v-icon>mdi-airport</v-icon>
-                <span style="margin-left: 15px;margin-right: 20px">FZU</span>
+                <span style="margin-left: 15px; margin-right: 15px">FZU</span>
                 <v-icon>mdi-arrow-right</v-icon>
-                <span style="margin-left: 20px">KUL</span>
+                <span style="margin-left: 15px">KUL</span>
               </p>
             </v-col>
-            <v-col cols="6" md="4" align-self="center">
-              <p style="font-size: 13px;font-weight: bold; margin-bottom: 5px">
+            <v-col  md="3" sm="2" align-self="center">
+              <p style="font-size: 15px;font-weight: bold;">
                 <v-icon>mdi-account-supervisor</v-icon>
                 <span style="margin-left: 15px; margin-right: 20px">20</span>
               </p>
