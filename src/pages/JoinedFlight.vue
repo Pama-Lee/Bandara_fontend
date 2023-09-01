07d554770483168db544322546b7127a64cb5e3a
Pama Lee <pama@pamalee.cn>
2023-09-01 16:22:09 +0800

    fix: 修复冲突

--- src/pages/JoinedFlight.vue
+++ src/pages/JoinedFlight.vue
@@ -3,10 +3,14 @@
         <v-card style="margin: 5vw; padding: 20px;">
             <span style="font-size: 25px; font-weight: bold;">已加入航班</span>
             <v-divider thickness="3" color="rgba(0,0,0)" style="margin-top: 20px;margin-bottom: 20px;"></v-divider>
-            <v-banner v-for="item in flight" :key="item.name">
-                    <v-row>
-                        <v-col cols="3">
-                            <img :src="'https://daisycon.io/images/airline/?width=300&height=300&iata='+item.name.substring(0, 2)" style="
+            <div id="loading" v-if="loading" style="text-align: center;">
+                <v-progress-circular indeterminate size="40"></v-progress-circular>
+            </div>
+            <v-banner v-for="item in flightList" :key="item.uuid" @click="go2Detail(item)">
+                <v-row>
+                    <v-col cols="3">
+                        <img :src="'https://daisycon.io/images/airline/?width=300&height=300&iata=' + item?.flightNumber.substring(0, 2)"
+                            style="
                                 border-radius: 50%;
                                 padding: 5px;
                                 background-color: rgba(113, 113, 113, 0.05); 
@@ -15,66 +19,68 @@
                                 width: 100%;
                                 height: 100%;
                                 box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.3);
-                            "/>
-                        </v-col>
-                        <v-col cols="9" align-self="center">
-                            <v-row>
-                                <v-col cols="6" align-self="center">
-                                    <v-row>
-                                        <h2>
-                                        <span style="margin-left: 15px;">{{ item.name }}</span>
-                                        </h2>
-                                    </v-row>
-                                    <v-row>
-                                        <h3>
-                                            <span style="margin-left: 15px;">{{ item.time.substring(0,5) }}</span>
-                                        </h3>
-                                        <br v-if="width < 600">
-                                        <h3>
-                                            <span style="margin-left: 15px;">{{ item.time.substring(6) }}</span>
-                                        </h3>
-                                    </v-row>
-                                </v-col>
-                                <v-col cols="6" align-self="center">
-                                    <span>航班状态： </span>
-                                    <br v-if="width < 600">
-                                    <span style="font-weight: bold;">{{ item.state }}</span>
-                                </v-col>
-                            </v-row>
-                        </v-col>
-                    </v-row>
+                            " />
+                    </v-col>
+                    <v-col cols="9" align-self="center">
+                        <v-row>
+                            <v-col cols="6" align-self="center">
+                                <v-row>
+                                    <h2>
+                                        <span style="margin-left: 15px;">{{ item.flightNumber }}</span>
+                                    </h2>
+                                </v-row>
+                                <v-row>
+                                    <h3 style="">
+                                        <span style="margin-left: 15px;">
+                                            {{new Date(item?.departureTime).toTimeString().substring(0, 5) }} 
+                                            <br v-if="width < 600">
+                                        {{new Date(item?.departureTime).toLocaleDateString()
+                                         }}</span>
+                                    </h3>
+                                </v-row>
+                            </v-col>
+                            <v-col cols="6" align-self="center">
+                                <span>航班状态： </span>
+                                <br v-if="width < 600">
+                                <span style="font-weight: bold;">{{"等待中"}}</span>
+                            </v-col>
+                            
+                        </v-row>
+                    </v-col>
+                </v-row>
+
             </v-banner>
         </v-card>
     </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
-//测试填充数据
-const flight = [
-    { 
-        name: "AK7100",
-        time: "14:25 2023/09/05",
-        state:"等待中"
-    },
-    { 
-        name: "MU7120",
-        time: "06:56 2023/07/05",
-        state:"已着陆"
-    },
-    { 
-        name: "CA6593",
-        time: "04:50 2023/09/05",
-        state:"等待中"
-    },
-    { 
-        name: "MF1200",
-        time: "16:55 2023/09/15",
-        state:"等待中"
-    }
-]
+import { getUserFlightList } from '@/services/user/user';
+import { ref } from 'vue';
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
 
+const flightList = ref([])
+
+const router = useRouter()
+const loading = ref(true)
 const width = ref(window.innerWidth)
+
+
+const go2Detail = (item: any) => {
+    router.push("/detail/" + item.uuid)
+}
+
+onMounted(() => {
+    getUserFlightList(null).then(res => {
+        if (res.code == 1) {
+            flightList.value = res.data
+            loading.value = false
+        } else {
+            alert("获取航班列表失败，请稍后再试")
+        }
+    })
+})
 
 // 当屏幕大小改变时
 onMounted(() => {
@@ -88,13 +94,13 @@ onMounted(() => {
 
 <style scoped>
 .background {
-  height: 100vh;
-  width: 100%;
-  overflow: auto;
-  background-image: url("https://api.oyohen.com/bing.php");
-  background-blend-mode: multiply;
-  background-size: cover;
-  background-position: center;
-  background-color: rgba(0, 0, 0, 0.5);
+    height: 100vh;
+    width: 100%;
+    overflow: auto;
+    background-image: url("https://api.oyohen.com/bing.php");
+    background-blend-mode: multiply;
+    background-size: cover;
+    background-position: center;
+    background-color: rgba(0, 0, 0, 0.5);
 }
 </style>
